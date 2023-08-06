bf273911b2b2b7ce96dd20ac7c20326696357a70
miyasitananami <141516957+miyasitananami@users.noreply.github.com>
2023-08-06 14:22:03 +0900

    Add files via upload
    initial commit

---
+++ myscript.ps1
@@ -0,0 +1,67 @@
+
+Ubuntu は既にインストールされています。
+Ubuntu を起動しています...
+To run a command as administrator (user "root"), use "sudo <command>".
+See "man sudo_root" for details.
+
+inoki@emiri-f:~$ npm create astro@latest
+Need to install the following packages:
+  create-astro@3.1.11
+Ok to proceed? (y) y
+
+╭─────╮  Houston:
+│ ◠ ◡ ◠  Let's build something great!
+╰─────╯
+
+ astro   v2.10.1 Launch sequence initiated.
+
+   dir   Where should we create your new project?
+         ./emily
+
+  tmpl   How would you like to start your new project?
+         Include sample files
+      ✔  Template copied
+
+  deps   Install dependencies?
+         Yes
+      ✔  Dependencies installed
+
+    ts   Do you plan to write TypeScript?
+         Yes
+
+   use   How strict should TypeScript be?
+         Strict
+      ✔  TypeScript customized
+
+   git   Initialize a new git repository?
+         Yes
+      ✔  Git initialized
+
+  next   Liftoff confirmed. Explore your project!
+
+         Enter your project directory using cd ./emily
+         Run npm run dev to start the dev server. CTRL+C to stop.
+         Add frameworks like react or tailwind using astro add.
+
+         Stuck? Join us at https://astro.build/chat
+
+╭─────╮  Houston:
+│ ◠ ◡ ◠  Good luck out there, astronaut! 🚀
+╰─────╯
+npm notice
+npm notice New minor version of npm available! 9.6.7 -> 9.8.1
+npm notice Changelog: https://github.com/npm/cli/releases/tag/v9.8.1
+npm notice Run npm install -g npm@9.8.1 to update!
+npm notice
+inoki@emiri-f:~$ cd ./emily
+inoki@emiri-f:~/emily$  npm run dev
+
+> emily@0.0.1 dev
+> astro dev
+
+  🚀  astro  v2.10.1 started in 99ms
+
+  ┃ Local    http://localhost:3000/
+  ┃ Network  use --host to expose
+
+12:39:57 PM [astro] reload /src/pages/index.astro (x3)
